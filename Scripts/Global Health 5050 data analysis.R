b11af83974e3824fa27030f0bf9b0b998b5a01ed
Lorenz <lorenznoe@gmail.com>
2020-12-08 17:40:17 -0500

    Add in code to estimate share of sex-disaggregated cases and deaths by month

--- Scripts/Global Health 5050 data analysis.R
+++ Scripts/Global Health 5050 data analysis.R
@@ -313,6 +313,97 @@ gh5050_historical <- gh5050_historical_raw %>%
 
 ### ANALYSIS ####
 
+# Looking at average monthly new cases per month and use gh5050_historical
+# to see if they're sex-disaggregated
+
+avg_new_cases <- owid_raw %>%
+  # Take out World aggregate and "International", which is no longer used (was for cruise ships, etc.)
+  filter(!location %in% c("World", "International")) %>%
+  # Clean country name variables
+  # Keep only variables we want
+  select(iso3c = iso_code, country = location, date, new_cases, new_deaths) %>%
+  # replace iso3 value for Kosovo with version that World Bank uses for merging.
+  mutate(iso3c = case_when(
+    country == "Kosovo" ~ "XKX",
+    TRUE ~ iso3c
+  )) %>%
+  mutate(month = lubridate::month(date)) %>%
+  group_by(iso3c, month) %>%
+  summarize(mean_new_cases = mean(new_cases, na.rm = TRUE),
+            mean_new_deaths = mean(new_deaths, na.rm = TRUE)) %>%
+  ungroup() %>%
+  # Sex-disaggregated cases yes/no
+  left_join(gh5050_historical %>%
+              as_tibble() %>%
+              mutate(month = lubridate::month(date_cases)) %>%
+              filter(!is.na(month)) %>%
+              distinct(iso3c, month) %>%
+              mutate(sex_disaggregated_cases = 1)) %>%
+  # Sex-disaggregated deaths yes/no
+  left_join(gh5050_historical %>%
+              as_tibble() %>%
+              mutate(month = lubridate::month(date_deaths)) %>%
+              filter(!is.na(month)) %>%
+              distinct(iso3c, month) %>%
+              mutate(sex_disaggregated_deaths = 1))
+
+# Cases over time
+avg_new_cases %>%
+  group_by(month, sex_disaggregated_cases) %>%
+  summarize(sum_cases = sum(mean_new_cases, na.rm = TRUE)) %>%
+  ungroup() %>%
+  pivot_wider(id_cols = month, names_from = sex_disaggregated_cases, values_from = sum_cases, names_prefix = "SDD_") %>%
+  mutate(share_SDD = SDD_1/(SDD_1 + SDD_NA),
+         share_NA = SDD_NA/(SDD_1 + SDD_NA)) %>%
+  select(-c(SDD_NA, SDD_1)) %>%
+  pivot_longer(cols = -c(month), names_to = "sdd", values_to = "share") %>%
+  mutate(sdd = case_when(
+    sdd == "share_NA" ~ "No",
+    TRUE ~ "Yes"
+  )) %>%
+  filter(month >= 3) %>%
+  ggplot(aes(x = month, y = share*100, fill = sdd)) +
+  geom_col() +
+  scale_x_continuous(breaks = c(3, 4, 5, 6, 7, 8, 9, 10, 11),
+                     labels = c(3, 4, 5, 6, 7, 8, 9, 10, 11)) +
+  scale_y_continuous(expand = expand_scale(mult = c(0, .1))) +
+  labs(x = "Month of 2020", y = "Share of cases",
+       title = "Cases",
+       caption = "Source: GH5050, ODW calculations",
+       fill = "Sex-disaggregated?") +
+  theme(panel.background = element_blank(), panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(linetype = "dotted", size=.1, color="gray" ), axis.line = element_line(colour = "black"))
+ggsave("Output/Share of cases disaggregated by sex.png", dpi = 600)
+
+
+# Deaths over time
+avg_new_cases %>%
+  group_by(month, sex_disaggregated_deaths) %>%
+  summarize(sum_deaths = sum(mean_new_deaths, na.rm = TRUE)) %>%
+  ungroup() %>%
+  pivot_wider(id_cols = month, names_from = sex_disaggregated_deaths, values_from = sum_deaths, names_prefix = "SDD_") %>%
+  mutate(share_SDD = SDD_1/(SDD_1 + SDD_NA),
+         share_NA = SDD_NA/(SDD_1 + SDD_NA)) %>%
+  select(-c(SDD_NA, SDD_1)) %>%
+  pivot_longer(cols = -c(month), names_to = "sdd", values_to = "share") %>%
+  mutate(sdd = case_when(
+    sdd == "share_NA" ~ "No",
+    TRUE ~ "Yes"
+  )) %>%
+  filter(month >= 3) %>%
+  ggplot(aes(x = month, y = share*100, fill = sdd)) +
+  geom_col() +
+  scale_x_continuous(breaks = c(3, 4, 5, 6, 7, 8, 9, 10, 11),
+                     labels = c(3, 4, 5, 6, 7, 8, 9, 10, 11)) +
+  scale_y_continuous(expand = expand_scale(mult = c(0, .1))) +
+  labs(x = "Month of 2020", y = "Share of deaths",
+       title = "Deaths",
+       caption = "Source: GH5050, ODW calculations",
+       fill = "Sex-disaggregated?") +
+  theme(panel.background = element_blank(), panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(linetype = "dotted", size=.1, color="gray" ), axis.line = element_line(colour = "black"))
+ggsave("Output/Share of deaths disaggregated by sex.png", dpi = 600)
+
 ###
 ### Can use gh5050_historical for this now, ignore below until Table 1
 ###
